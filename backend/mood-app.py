e225e2b4dbfcf48c80df95008927950bde127d36
Emil <eamuri01@louisville.edu>
2026-03-13 07:35:59 -0400

    changed stack of project and created the frontend and backend

---
+++ backend/mood-app.py
@@ -0,0 +1,22 @@
+# #bunny dark moon bg: https://pbs.twimg.com/media/GjiY-gpa0AACikk?format=png&name=large
+# import streamlit as st
+
+# st.markdown(
+#     """
+#     <style>
+#     [data-testid="stAppViewContainer"] {
+#         background-image: url("https://i.pinimg.com/736x/22/3b/1a/223b1a1767665c01eafa37c959adef83.jpg");
+#         background-size: cover;
+#     }
+
+#     [data-testid="stSidebar"] {
+#         background-image: url("https://i.pinimg.com/736x/4c/38/f9/4c38f9236067caf05eab3acdf30bd046.jpg");
+#         background-size: cover;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+# st.markdown("# :red[Mood Tracker App] :rabbit2::heart:")
+
